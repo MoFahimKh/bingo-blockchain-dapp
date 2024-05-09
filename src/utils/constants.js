5ac47aaedf12d8f8cd3275d50a25e984b5400a20
MfahimKh <122276720+MoFahimKh@users.noreply.github.com>
2024-05-09 13:51:23 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 export const TEST_ERC20 = {
-  name: "T",
-  symbol: "T",
-  decimal: 18,
-  address: "0xF5968a02084C6221F8eeD45EB1AcB3BaE6F59F4b", 
+  name: "Testnet fUSDT",
+  symbol: "fUSDT",
+  decimal: 6,
+  address: "0x633b94d51cC86DCBc34344E782A89Fd8c6de5915", 
 };
 
-export const CHAIN_ID = "0x13881"; 
+export const CHAIN_ID = "0x431"; 
 
 
-export const BINGO_CONTRACT_ADD = "0xc08C3bC01C180ce83a2D42B690B903d63C5BD96E"
+export const BINGO_CONTRACT_ADD = "0xfE92Da59D238270B6Ec238931653490A4139A8ef"
